ec9c2c896b28cdb2b93b660ab537fde82933ddbc
Atul Singh <46633779+hipswan@users.noreply.github.com>
2020-07-25 02:48:02 +0530

    removed eslint enforced rules

---
+++ android/app/src/main/java/com/touchcode/poloroid/Application.java
@@ -0,0 +1,33 @@
+package com.touchcode.poloroid;
+
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingPlugin;
+
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+
+public class Application extends FlutterApplication implements PluginRegistrantCallback {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    FlutterFirebaseMessagingService.setPluginRegistrant(this);
+  }
+
+  @Override
+  public void registerWith(PluginRegistry registry) {
+    FirebaseCloudMessagingPluginRegistrant.registerWith(registry);
+  }
+}
